82e18358727a91a26345b38f0631baeb51c49cad
vasukihn <48438013+vasukihn@users.noreply.github.com>
2025-03-12 13:31:51 +0000

    context_printer_task_id

--- dags/Context Printer.py
+++ dags/Context Printer.py
@@ -12,6 +12,4 @@ with DAG(
     end_date=datetime(year=2025, month=3, day=2),
     schedule="@daily",
 ):
-    print_context_var = PythonOperator(task_id="demo_templating", python_callable=print_context)
-
-    print_context_var
+    print_context_var = PythonOperator(task_id="context_printer_task_id", python_callable=print_context)
