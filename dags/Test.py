1eb90025716ef1a89f2973cc98070f9d28c3fe94
vasukihn <48438013+vasukihn@users.noreply.github.com>
2025-03-12 12:07:41 +0000

    add launch dag

--- dags/Test.py
+++ dags/Test.py
@@ -3,15 +3,20 @@ from datetime import datetime
 from airflow.models import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
+form airflow.operators.empty import EmptyOperator
 
 with DAG(
-    dag_id="00_testing",
+    dag_id="launch_dag",
     start_date=datetime(year=2025, month=3, day=1),
     end_date=datetime(year=2025, month=3, day=2),
     schedule="@daily",
 ):
-    hello = BashOperator(task_id="hello", bash_command="echo 'hello'")
-    
-    world = PythonOperator(task_id="world", python_callable=lambda: print("world"))
+    procure_rocket_material = EmptyOperator(task_id="procure_rocket_material")
+    procure_fuel = EmptyOperator(task_id="procure_fuel")
+    build_stage_1 = EmptyOperator(task_id="build_stage_1")
+    build_stage_2 = EmptyOperator(task_id="build_stage_2")
+    build_stage_3 = EmptyOperator(task_id="build_stage_3")
+    launch = EmptyOperator(task_id="launch")
 
-    world >> hello
+    procure_rocket_material >> [build_stage_1, build_stage_2, build_stage_3] >> launch
+    procure_fuel >> build_stage_3 >> launch
